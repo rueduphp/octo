38f1cab80560c0e07a073f301363f15284ba3f9d
Gérald Plusquellec <gplusquellec@gmail.com>
2017-11-17 08:14:11 +0100

    eventests

--- lib/listener.php
+++ lib/listener.php
@@ -1,40 +1,170 @@
 <?php
-    namespace Octo;
+namespace Octo;
 
-    class Listener implements FastListenerInterface
-    {
-        public $priority    = 0;
-        public $halt        = false;
-        public $once        = false;
-        public $called      = false;
-        public $callable;
+/**
+ * Class Listener
+ * @package Octo
+ */
+class Listener implements FastListenerInterface
+{
+    /**
+     * @var int
+     */
+    public $priority    = 0;
 
-        public function __construct(callable $callable, $priority = 0, $halt = false, $once = false)
-        {
-            $this->priority = $priority;
-            $this->callable = $callable;
-            $this->halt     = $halt;
-            $this->once     = $once;
-        }
+    /**
+     * @var bool
+     */
+    public $halt        = false;
 
-        public function once()
-        {
-            $this->once = !$this->once;
+    /**
+     * @var bool
+     */
+    public $once        = false;
 
-            return $this;
-        }
+    /**
+     * @var bool
+     */
+    public $called      = false;
 
-        public function halt(bool $halt)
-        {
-            $this->halt = $halt;
+    /**
+     * @var callable
+     */
+    public $callable;
 
-            return $this;
-        }
-
-        public function called()
-        {
-            $this->called = !$this->called;
-
-            return $this;
-        }
+    /**
+     * @param callable $callable
+     * @param int $priority
+     * @param bool $halt
+     * @param bool $once
+     */
+    public function __construct(
+        callable $callable,
+        $priority = 0,
+        $halt = false,
+        $once = false
+    ) {
+        $this->priority = $priority;
+        $this->callable = $callable;
+        $this->halt     = $halt;
+        $this->once     = $once;
     }
+
+    /**
+     * @return $this
+     */
+    public function once()
+    {
+        $this->once = !$this->once;
+
+        return $this;
+    }
+
+    /**
+     * @param bool $halt
+     *
+     * @return $this
+     */
+    public function halt(bool $halt)
+    {
+        $this->halt = $halt;
+
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    public function called()
+    {
+        $this->called = !$this->called;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPriority()
+    {
+        return $this->priority;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isHalt()
+    {
+        return $this->halt;
+    }
+
+    /**
+     * @param bool $halt
+     * @return $this
+     */
+    public function setHalt(bool $halt)
+    {
+        $this->halt = $halt;
+
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isOnce()
+    {
+        return $this->once;
+    }
+
+    /**
+     * @param bool $once
+     *
+     * @return Listener
+     */
+    public function setOnce(bool $once)
+    {
+        $this->once = $once;
+
+        return $this;
+    }
+
+    /**
+     * @param bool $called
+     * @return Listener
+     */
+    public function setCalled(bool $called)
+    {
+        $this->called = $called;
+
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isCalled()
+    {
+        return $this->called;
+    }
+
+    /**
+     * @return callable
+     */
+    public function getCallable()
+    {
+        return $this->callable;
+    }
+
+    /**
+     * @param callable $callable
+     *
+     * @return Listener
+     */
+    public function setCallable(callable $callable)
+    {
+        $this->callable = $callable;
+
+        return $this;
+    }
+}
--- tests/EventsTest.php
+++ tests/EventsTest.php
@@ -14,28 +14,33 @@ class EventsTest extends TestCase
         $this->manager = $this->getEventManager();
 
         $event = $this->manager->on('test', function ($num) {
-            Octo\set('evval', Octo\get('evval') + $num);
+            Octo\set('eventval', Octo\get('eventval') + $num);
         });
 
         $event->halt(true);
 
         $this->manager->on('test2', function ($num) {
-            Octo\set('evval', Octo\get('evval') + $num);
+            Octo\set('eventval', Octo\get('eventval') + $num);
         });
     }
 
     public function testFire()
     {
-        Octo\set('evval', 5);
+        Octo\set('eventval', 5);
 
         $this->assertTrue($this->manager->has('test'));
 
         $this->manager->fire('test', 10);
-        $this->manager->fire('test', 10);
-        $this->manager->fire('test2', 100);
-        $this->manager->fire('test2', 100);
+        $this->assertEquals(15, Octo\get('eventval'));
 
-        $this->assertEquals(15, Octo\get('evval'));
+        $this->manager->fire('test', 10);
+        $this->assertEquals(15, Octo\get('eventval'));
+
+        $this->manager->fire('test2', 100);
+        $this->assertEquals(15, Octo\get('eventval'));
+
+        $this->manager->fire('test2', 100);
+        $this->assertEquals(15, Octo\get('eventval'));
 
         $this->manager->delete('test');
         $this->assertFalse($this->manager->has('test'));
